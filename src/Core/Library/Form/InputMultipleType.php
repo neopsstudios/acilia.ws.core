e34fa2c09f1158a86ee0b5c6e275f5a2e5cb04b7
Javier Domínguez <jvrdom@gmail.com>
2020-05-12 11:49:30 -0300

    WST-157 | Crear tipo de formulario para trabajar con 'open' choices

--- src/Core/Library/Form/InputMultipleType.php
+++ src/Core/Library/Form/InputMultipleType.php
@@ -5,20 +5,12 @@ namespace WS\Core\Library\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class InputMultipleType extends AbstractType
 {
     const INPUT_MULTIPLE_TYPE_ATTR = [
         'data-component' => 'ws_input-multiple',
     ];
-
-    protected $translator;
-
-    public function __construct(TranslatorInterface $translator)
-    {
-        $this->translator = $translator;
-    }
 
     public function configureOptions(OptionsResolver $resolver)
     {

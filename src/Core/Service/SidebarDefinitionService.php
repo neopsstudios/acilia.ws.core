bd27331bc1317c6c63337e6e5c21284a8b0aef8a
Juan Antonio Cepeda Gómez <juan.antonio@acilia.es>
2020-05-18 16:04:07 +0200

    feature/WS-139-Ajustes-CMS

--- src/Core/Service/SidebarDefinitionService.php
+++ src/Core/Service/SidebarDefinitionService.php
@@ -16,7 +16,7 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
         $this->sidebarService = $sidebarService;
 
         // Set Default Logo
-        $this->sidebarService->assets->set('logo', 'images/logo-acilia.svg');
+        $this->sidebarService->assets->set('logo', '/cms/images/logo-acilia.svg');
     }
 
     public function getSidebarDefinition(): array
@@ -26,14 +26,14 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
             'dashboard',
             'dashboard.menu',
             [
-                'route_name' => 'ws_dashboard'
+                'route_name' => 'ws_dashboard',
             ],
             [
                 'roles' => [],
                 'translation_domain' => 'ws_cms',
                 'icon' => 'fa-tachometer-alt-fast',
                 'collapsed_routes' => ['ws_dashboard'],
-                'order' => 1
+                'order' => 1,
             ]
         );
 
@@ -42,14 +42,14 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
             'translations',
             'menu',
             [
-                'route_name' => 'ws_translation_index'
+                'route_name' => 'ws_translation_index',
             ],
             [
                 'roles' => ['ROLE_WS_CORE_TRANSLATION'],
                 'translation_domain' => 'ws_cms_translation',
                 'icon' => 'fa-language',
                 'collapsed_routes' => ['ws_translation_'],
-                'order' => 5
+                'order' => 5,
             ]
         );
 
@@ -64,7 +64,7 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
                 'translation_domain' => 'ws_cms_setting',
                 'icon' => 'fa-cogs',
                 'collapsed_routes' => ['ws_setting_'],
-                'order' => 6
+                'order' => 6,
             ]
         );
 
@@ -77,14 +77,14 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
                 $settingSectionDefinition->getName(),
                 [
                     'route_name' => 'ws_setting_index',
-                    'route_options' => ['section' => $settingSectionDefinition->getCode()]
+                    'route_options' => ['section' => $settingSectionDefinition->getCode()],
                 ],
                 [
                     'roles' => [$settingSectionDefinition->getRole()],
                     'translation_domain' => $settingSectionDefinition->getTranslationDomain(),
                     'icon' => $settingSectionDefinition->getIcon(),
                     'collapsed_routes' => ['ws_setting_'],
-                    'order' => $settingSectionDefinition->getOrder()
+                    'order' => $settingSectionDefinition->getOrder(),
                 ]
             ));
         }
@@ -94,14 +94,14 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
             'administrators',
             'menu',
             [
-                'route_name' => 'ws_administrator_index'
+                'route_name' => 'ws_administrator_index',
             ],
             [
                 'roles' => ['ROLE_WS_CORE_ADMINISTRATOR'],
                 'translation_domain' => 'ws_cms_administrator',
                 'icon' => 'fa-user-shield',
                 'collapsed_routes' => ['ws_administrator_'],
-                'order' => 7
+                'order' => 7,
             ]
         );
 
@@ -109,7 +109,7 @@ class SidebarDefinitionService implements SidebarDefinitionInterface
             $dashboardNode,
             $translationsNode,
             $settingsNode,
-            $administratorsNode
+            $administratorsNode,
         ];
     }
 }
